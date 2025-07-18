3afa907036f38c32139504dff1c048b04363e472
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-18 11:43:30 +0200

    Base Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import { useState } from "react";
+// import { useState } from "react";
+
+const singles = [
+  "Even in Arcadia",
+  "Chokehold",
+  "Granite",
+  "Gethsemane",
+  "The Summoning",
+  "Euclid",
+];
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <div className="container">
+      <ul className="singlesList">
+        {singles.map((single, index) => {
+          return (
+            <li key={index} className="single">
+              {single}
+            </li>
+          );
+        })}
+      </ul>
 
-  return <></>;
+      {/* Aggiungiamo in pagina un semplice form con un campo input in cui inserire il titolo di un nuovo articolo del blog.
+ Al submit del form, mostrare la lista degli articoli aggiornati. */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/assets/data/articles.js
@@ -0,0 +1,10 @@
+const singles = [
+  "Even in Arcadia",
+  "Chokehold",
+  "Granite",
+  "Gethsemane",
+  "The Summoning",
+  "Euclid",
+];
+
+export default singles;
